37d9bd9b7571bdeb7cc99eebd804a7df6df3e121
brenosekiro <b88660501@gmail.com>
2021-02-18 07:05:01 -0300

    homescren

--- lib/api.dart
+++ lib/api.dart
@@ -1,1 +1,31 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:youtube_favorites/models/video.dart';
+
 const String API_KEY = 'AIzaSyAi4M3qSaCbL5Jf1K39CPt3z-EwCMNBdjw';
+
+class Api {
+  search(String search) async {
+
+    http.Response response = await http.get("https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10");
+    decode(response);
+
+  }
+
+  List<Video> decode(http.Response response) {
+    if(response.statusCode == 200) {
+      dynamic decoded = json.decode(response.body);
+
+      List<Video> videos = decoded['items'].map<Video>((map) {
+        return Video.fromJson(map);
+      }).toList();
+
+      print(videos);
+
+      return videos;
+    } else {
+      throw Exception('Failed to load videos');
+    }
+  }
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 25,
+          child: Image.asset('images/yt.png'),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black87,
+        actions: [
+          Align(
+            alignment: Alignment.center,
+            child: Text('0')
+          ),
+          IconButton(
+            icon: Icon(Icons.star),
+            onPressed: () {
+
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              
+            },
+          )
+        ],
+      ),
+      body: Container(),
+    );
+  }
+}
