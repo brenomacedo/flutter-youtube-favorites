440a1aef3867b2bd7a1a5351d2ddec7cc97362c1
brenosekiro <b88660501@gmail.com>
2021-02-20 16:52:06 -0300

    appbar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,8 +3,33 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 25,
+          child: Image.asset('images/yt.png'),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black87,
+        actions: [
+          Align(
+            alignment: Alignment.center,
+            child: Text('0')
+          ),
+          IconButton(
+            icon: Icon(Icons.star),
+            onPressed: () {
+
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+
+            },
+          )
+        ],
+      )
     );
   }
 }
